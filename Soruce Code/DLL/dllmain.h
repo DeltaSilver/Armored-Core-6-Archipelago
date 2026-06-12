d844ebe5ca04e538fd94ab42fc41759ec2dc6e4a
Delta <64341149+DeltaSilver@users.noreply.github.com>
2026-06-12 02:22:00 +0100

    Delete Soruce Code directory

--- Soruce Code/DLL/dllmain.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void Log(const char* format, ...);
-void QueueGrant(int itemId);
-void SetGrantsSafe(bool safe);
-void SetGarageVisited();
--- Soruce Code/DLL/flagwriter.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cstdint>
-
-bool ReadEventFlag(uintptr_t eventFlagMan, int32_t divisor, uint32_t flagId);
-void WriteEventFlag(uintptr_t eventFlagMan, int32_t divisor,
-    uint32_t flagId, bool value);
